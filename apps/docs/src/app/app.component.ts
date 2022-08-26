c5853710d6d99dd94348d11e8721b3708965e6e7
andrew.poyntz <andrew.poyntz@metoffice.gov.uk>
2022-08-26 12:38:44 +0100

    basic update to docs showing component input changes

--- apps/docs/src/app/app.component.ts
+++ apps/docs/src/app/app.component.ts
@@ -7,4 +7,6 @@ import {Component} from '@angular/core';
 })
 export class AppComponent {
   title = 'docs';
+  impact = 2;
+  likelihood = 4;
 }
--- apps/docs/src/app/app.module.ts
+++ apps/docs/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {NswwsImpactMatrixAngularModule} from '@nx-test/nswws-impact-matrix-angular';
 import {defineCustomElements} from '../../../../dist/libs/nswws/impact-matrix/dist/loader';
+import {FormsModule} from '@angular/forms';
 
 defineCustomElements();
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, NswwsImpactMatrixAngularModule],
+  imports: [BrowserModule, NswwsImpactMatrixAngularModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
